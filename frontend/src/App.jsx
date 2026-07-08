85e6c68455a4602e10f9e7f28a4b41856feba8b7
isaac <ifhumail@gmail.com>
2026-07-09 00:51:28 +0200

    Add components folder + MovieCard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,11 @@ import "./App.css";
 function App() {
   return (
     <>
-      <Text display="hi" />
-      <Text display="world"></Text>
+      
     </>
   );
 }
 
-const Text = ({ display }) => {
-  return (
-    <div>
-      <p>{display}</p>
-    </div>
-  );
-};
+
 
 export default App;
