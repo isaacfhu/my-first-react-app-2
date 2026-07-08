7bfb691de0722a4f4f79e1e1d256e1d4d28a170e
isaac <ifhumail@gmail.com>
2026-07-09 00:55:22 +0200

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import "./App.css";
+import MovieCard from "./components/MovieCard";
 
 function App() {
   return (
     <>
-      
+      <MovieCard movie={{ title: "Tim Movie", release_date: "2026" }} />
+      <MovieCard movie={{ title: "Joe Movie", release_date: "2026" }} />
     </>
   );
 }
-
-
 
 export default App;
