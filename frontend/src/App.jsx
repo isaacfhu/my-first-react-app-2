7f8e7a1e71aa32cebc5bfd14a36335be5ef1cb7b
isaac <ifhumail@gmail.com>
2026-07-11 07:20:12 +0200

    update + creation of pages folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,9 @@
 import "./App.css";
-import MovieCard from "./components/MovieCard";
 
 function App() {
-  const movieNumber = 1
-
   return (
     <>
-    {movieNumber === 1 &&  (<MovieCard movie={{ title: "Tim Movie", release_date: "2026" }} />) }
-      
-      
+    
     </>
   );
 }
